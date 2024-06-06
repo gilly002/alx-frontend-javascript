3cc123f886de624aa63fa84b61e3e5267c4a89d7
gilly002 <gilbertamenorhu@gmail.com>
2024-06-06 03:08:08 +0000

    0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -4,8 +4,8 @@
  */
 export default function getListStudents() {
   return [
-    { id: 1, firstName: 'Gilly', location: 'San Francisco' },
-    { id: 2, firstName: 'Gray', location: 'Columbia' },
-    { id: 5, firstName: 'Selorm', location: 'San Francisco' },
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
 }
